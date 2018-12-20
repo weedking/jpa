4e8cc87f1693b18cebc61c66401d49ade0a4d09a
weedking <260815998@qq.com>
2018-12-20 22:40:17 +0800

    前后端打通，前端输入数据，跳转到新页面，从数据库读出数据并显示。

--- src/main/java/com/billy/jpa/controller/MyController.java
+++ src/main/java/com/billy/jpa/controller/MyController.java
@@ -7,7 +7,11 @@ import com.billy.jpa.pojo.Department;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 //@Controller
 //@ResponseBody
@@ -26,6 +30,7 @@ public class MyController {
     @GetMapping(path = "/department")
     public @ResponseBody String addDepartment(@RequestParam Long did
             , @RequestParam String dname){
+        //参数名与html一致即可存入数据库
         Department d = new Department();
         d.setId(did);
         d.setName(dname);
@@ -34,17 +39,30 @@ public class MyController {
     }
 
 
-    @GetMapping(path="/allDepartment")
-    public @ResponseBody Iterable<Department> getAllDepartment() {
-        // This returns a JSON or XML with the users
-        return departmentRepository.findAll();
-    }
+//    @GetMapping(path="/allDepartment")
+//    public @ResponseBody Iterable<Department> getAllDepartment(ModelMap modelMap) {
+//        // This returns a JSON or XML with the users
+//        List<Department> de = departmentRepository.findAll();
+//        modelMap.put("de",de);
+//        return de;
+////        return departmentRepository.findAll();
+//    }
 
     @RequestMapping("/t2")
     public String getHello(){
 
         return "999";
     }
+
+    @RequestMapping(value = "/greeting")
+    public ModelAndView test(ModelAndView mv,ModelMap modelMap) {
+        mv.setViewName("/greeting");
+        mv.addObject("title","欢迎使用Thymeleaf!");
+        List<Department> de = departmentRepository.findAll();
+        modelMap.put("de",de);
+        return mv;
+    }
+
 
 
 }
